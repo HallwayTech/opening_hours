0c8771d8bde575f0d07627294510d0d056c70f7e
Mikkel Hoegh <mikkel@hoegh.org>
2011-11-26 15:54:45 +0100

    Getting ready for the JavaScript work.

---
+++ templates/opening_hours_admin.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file
+ * Template for the opening hours admin interface.
+ *
+ * Is not really a template in Drupal-sense, mainly a container for the
+ * markup necessary to render the opening hours interface.
+ */
+?>
+<div id="opening-hours-admin">
+  <p class="placeholder"><?php print t('Loading administration interface…'); ?></p>
+</div>
+
