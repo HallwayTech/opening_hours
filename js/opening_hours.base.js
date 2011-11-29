30206ef7b5e8f33c35ee75adf1e0e3ea6a59e650
Mikkel Hoegh <mikkel@hoegh.org>
2011-11-29 20:17:50 +0100

    Implemented simple date formatter.

--- js/opening_hours.base.js
+++ js/opening_hours.base.js
@@ -123,5 +123,58 @@ Drupal.OpeningHours.Week = function (dateStr, firstDayOfWeek) {
   self.constructor();
 };
 
+/**
+ * Format a date.
+ */
+Drupal.OpeningHours.formatDate = function (date) {
+  return $.datepicker.formatDate('d. MM yy', date, Drupal.settings.OpeningHours.formatDate);
+};
+
+/**
+ * Format a date range.
+ *
+ * Filters the range down to the significant parts, so only the things
+ * that are different are shown in the first part of the range.
+ */
+Drupal.OpeningHours.formatDateRange = function (date1, date2) {
+  var d1 = {
+        year: date1.getFullYear(),
+        month: date1.getMonth(),
+        day: date1.getDate(),
+      },
+      d2 = {
+        year: date2.getFullYear(),
+        month: date2.getMonth(),
+        day: date2.getDate(),
+      };
+
+  // If it's the same date, just return that.
+  if (d1.year === d2.year && d1.month === d2.month && d1.day === d2.day) {
+    return [Drupal.OpeningHours.formatDate(date1)];
+  }
+  // If it's the same month, return a simple day-range.
+  else if (d1.year === d2.year && d1.month === d2.month) {
+    return [
+      $.datepicker.formatDate('d.', date1, Drupal.settings.OpeningHours.formatDate),
+      Drupal.OpeningHours.formatDate(date2)
+    ];
+  }
+  // If it's the same year, return a day-month range.
+  else if (d1.year === d2.year) {
+    return [
+      $.datepicker.formatDate('d. MM', date1, Drupal.settings.OpeningHours.formatDate),
+      Drupal.OpeningHours.formatDate(date2)
+    ];
+  }
+  // Otherwise, return the full date range.
+  else {
+    return [
+      Drupal.OpeningHours.formatDate(date1),
+      Drupal.OpeningHours.formatDate(date2)
+    ];
+  }
+};
+
 }(jQuery));
+
 
