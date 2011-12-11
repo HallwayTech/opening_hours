50762e39f8cfb105e0b2f4ba9320372e7aefc9d6
Mikkel Hoegh <mikkel@hoegh.org>
2011-12-11 01:53:49 +0100

    Splitting prototype extensions into its own file.
    Renamed base to core, and fixed indentation.

---
+++ js/opening_hours.core.js
@@ -0,0 +1,135 @@
+/**
+ * @file
+ * Contains the base Drupal.OpeningHours object with utility functions.
+ */
+
+(function(){
+  "use strict";
+
+  Drupal.OpeningHours = {};
+
+  // Sunday is either the zeroeth or seventh day of the week, depending
+  // oun the current locale.
+  var weekdays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+  Drupal.OpeningHours.Week = function (dateStr, firstDayOfWeek) {
+    var self = this;
+
+    self.constructor = function () {
+      self.dateStr = dateStr || new Date().getISODate();
+      self.firstDayOfWeek = firstDayOfWeek;
+      self.weekDays = self.orderedWeekDays(self.firstDayOfWeek);
+      self.dates = self.getDates();
+    };
+
+    // Find all the dates our week spans over.
+    self.getDates = function () {
+      var date = new Date().setISODate(self.dateStr),
+          dates = [],
+          dayOffset = 0,
+          tempDate,
+          todaysDayNumber = date.getDay();
+  
+      // Determine how far back in time the first day of the week was.
+      // This is a bit weird, since the current day number might be larger
+      // than the first day number.
+      // If today's number is larger, this is fairly easy.
+      if (todaysDayNumber < self.firstDayOfWeek) {
+        dayOffset = self.firstDayOfWeek - 7 + todaysDayNumber;
+      }
+      // In the other case, we reverse the order of the subtraction.
+      else if (todaysDayNumber > self.firstDayOfWeek) {
+        dayOffset = self.firstDayOfWeek - todaysDayNumber;
+      }
+  
+      while (dates.length < 7) {
+        tempDate = new Date().setISODate(self.dateStr);
+  
+        tempDate.setDate(tempDate.getDate() + dayOffset);
+  
+        dates.push(tempDate);
+        dayOffset += 1;
+      }
+  
+      return dates;
+    };
+
+    /**
+    * Get the weekdays in the order.
+    *
+    * The day number (0-6) given will be the first. Zero is sunday.
+    */
+    self.orderedWeekDays = function () {
+      var counter = self.firstDayOfWeek,
+          order = [];
+
+      while (order.length < 7) {
+        order.push(weekdays[counter % 7]);
+        counter = counter + 1;
+      }
+
+      return order;
+    };
+
+    self.constructor();
+  };
+
+  /**
+  * Format a date.
+  */
+  Drupal.OpeningHours.formatDate = function (date, format) {
+    // Default format.
+    if (!format) {
+      format = 'd. MM yy';
+    }
+
+    return $.datepicker.formatDate(format, date, Drupal.settings.OpeningHours.formatDate);
+  };
+
+  /**
+  * Format a date range.
+  *
+  * Filters the range down to the significant parts, so only the things
+  * that are different are shown in the first part of the range.
+  */
+  Drupal.OpeningHours.formatDateRange = function (date1, date2) {
+    var d1 = {
+          year: date1.getFullYear(),
+          month: date1.getMonth(),
+          day: date1.getDate(),
+        },
+        d2 = {
+          year: date2.getFullYear(),
+          month: date2.getMonth(),
+          day: date2.getDate(),
+        };
+
+    // If it's the same date, just return that.
+    if (d1.year === d2.year && d1.month === d2.month && d1.day === d2.day) {
+      return [Drupal.OpeningHours.formatDate(date1)];
+    }
+    // If it's the same month, return a simple day-range.
+    else if (d1.year === d2.year && d1.month === d2.month) {
+      return [
+        $.datepicker.formatDate('d.', date1, Drupal.settings.OpeningHours.formatDate),
+        Drupal.OpeningHours.formatDate(date2)
+      ];
+    }
+    // If it's the same year, return a day-month range.
+    else if (d1.year === d2.year) {
+      return [
+        $.datepicker.formatDate('d. MM', date1, Drupal.settings.OpeningHours.formatDate),
+        Drupal.OpeningHours.formatDate(date2)
+      ];
+    }
+    // Otherwise, return the full date range.
+    else {
+      return [
+        Drupal.OpeningHours.formatDate(date1),
+        Drupal.OpeningHours.formatDate(date2)
+      ];
+    }
+  };
+
+}());
+
